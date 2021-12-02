5ead0e3f7c35e8f7ef528e25a611813295a0d805
AshishShresth <57864980+AshishShresth@users.noreply.github.com>
2021-12-02 07:49:56 +0545

    Included form label

--- script.js
+++ script.js
@@ -67,3 +67,25 @@ const toggleButton = document.getElementsByClassName('toggle-button')[0];
 toggleButton.addEventListener('click', () => {
   navHeader.classList.toggle('active');
 });
+
+// navigation highlight
+const sections = document.querySelectorAll('section');
+const navLi = document.querySelectorAll('.nav ul li');
+
+window.addEventListener('scroll', () => {
+  let current = '';
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute('id');
+    }
+  });
+
+  navLi.forEach((li) => {
+    // li.classList.remove('active');
+    if (li.classList.contains(current)) {
+      li.classList.add('active');
+    }
+  });
+});
